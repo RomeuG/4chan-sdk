812da762cb0afa3bcafd87aa4ed27c7ebfcfc9a5
Romeu Vieira <romeu.bizz@gmail.com>
2019-12-31 22:46:49 +0000

    Initial commit for Post struct

---
+++ include/Post.hpp
@@ -0,0 +1,14 @@
+#ifndef _POST_HPP_
+#define _POST_HPP_
+
+#include <string>
+
+struct Post
+{
+	std::string text;
+
+	Post();
+	~Post();
+};
+
+#endif
---
+++ src/Post.cpp
@@ -0,0 +1,6 @@
+#include "Post.hpp"
+
+Post::Post()
+{
+
+}
