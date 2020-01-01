91c99feed829688b6a8e4c70102e36c8ba9524e9
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-01 17:20:09 +0000

    Post class

--- include/Post.hpp
+++ include/Post.hpp
@@ -1,14 +1,32 @@
 #ifndef _POST_HPP_
 #define _POST_HPP_
 
+#include <File.hpp>
+#include <optional>
 #include <string>
 
-struct Post
-{
-	std::string text;
+struct Post {
+    std::string postnumber;
+    std::string time;
+    std::string name;
+    std::string subject;
+    std::string tripcode;
 
-	Post();
-	~Post();
+    std::optional<File> file;
+
+    std::string thread;
+    std::string id;
+    std::string text;
+    std::string country;
+    std::string country_name;
+
+    std::string replies;
+    std::string images;
+    std::string unique_ips;
+    std::string tail_size;
+
+    Post();
+    ~Post();
 };
 
 #endif
