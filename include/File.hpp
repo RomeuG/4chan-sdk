f71e5909099c6c7ab4cfae3fffac1493aff22ad0
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-01 12:52:17 +0000

    File class

---
+++ include/File.hpp
@@ -0,0 +1,19 @@
+#ifndef _FILE_HPP_
+#define _FILE_HPP_
+
+#include <string>
+
+struct File {
+    std::string filename;
+    std::string fileext;
+    std::string filew;
+    std::string fileh;
+    std::string filetw;
+    std::string fileth;
+    std::string tim;
+    std::string time;
+    std::string md5;
+    std::string fsize;
+};
+
+#endif
