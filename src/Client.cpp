4c037f4a2be89152f3037c25c18c90ed800d4f0e
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-01 22:42:32 +0000

    Map post values to struct

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,17 +1,73 @@
 #include <Client.hpp>
 
+#define GET_VAL(a, b, c, d) \
+    if (!a[b].empty()) {    \
+        c = a[b].get<d>();  \
+    }
+
+#define GET_VAL_STR(a, b, c)         \
+    if (!a[b].empty()) {             \
+        c = a[b].get<std::string>(); \
+    }
+
+#define GET_VAL_INT(a, b, c) \
+    if (!a[b].empty()) {     \
+        c = a[b].get<int>(); \
+    } else {                 \
+        c = -1;              \
+    }
+
+auto _get_file(nlohmann::json& post) -> File
+{
+    File file;
+
+    file.name = post["filename"].get<std::string>();
+
+    GET_VAL(post, "ext", file.ext, std::string);
+    GET_VAL(post, "w", file.w, int);
+    GET_VAL(post, "h", file.h, int);
+
+    GET_VAL(post, "tn_w", file.tn_w, int);
+    GET_VAL(post, "tn_h", file.tn_h, int);
+
+    GET_VAL(post, "tim", file.tim, int);
+    GET_VAL(post, "time", file.time, int);
+
+    GET_VAL(post, "md5", file.md5, std::string);
+
+    GET_VAL(post, "fsize", file.size, int);
+
+    return file;
+}
+
 // TODO: finish this function
 auto _get_post(nlohmann::json& post) -> Post
 {
     Post post_obj;
 
-    if (!post["no"].empty()) {
-        post_obj.postnumber = post["no"].get<int>();
+    GET_VAL(post, "no", post_obj.postnumber, int);
+    GET_VAL(post, "now", post_obj.date, std::string);
+    GET_VAL(post, "name", post_obj.name, std::string);
+    GET_VAL(post, "sub", post_obj.subject, std::string);
+
+    // TODO: comment (parse the html inside it)
+    GET_VAL(post, "com", post_obj.text, std::string);
+
+    // TODO: file
+    if (!post["filename"].empty()) {
+        auto file_obj = _get_file(post);
+        post_obj.file = file_obj;
     }
 
-    if (!post["name"].empty()) {
-        post_obj.name = post["name"].get<std::string>();
-    }
+    GET_VAL(post, "id", post_obj.id, std::string);
+    GET_VAL(post, "country", post_obj.country, std::string);
+    GET_VAL(post, "country_name", post_obj.country_name, std::string);
+    GET_VAL(post, "replies", post_obj.replies, int);
+    GET_VAL(post, "images", post_obj.images, int);
+    GET_VAL(post, "unique_ips", post_obj.unique_ips, int);
+    GET_VAL(post, "tail_size", post_obj.tail_size, int);
+    GET_VAL(post, "archived", post_obj.archived, int);
+    GET_VAL(post, "archived_on", post_obj.archived_on, int);
 
     return post_obj;
 }
