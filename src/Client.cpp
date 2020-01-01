a4f11cef58aeeaa15d603bc012efd61a302e23e4
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-01 18:01:24 +0000

    Client file (WIP)

---
+++ src/Client.cpp
@@ -0,0 +1,41 @@
+#include <Download.hpp>
+#include <Post.hpp>
+#include <Thread.hpp>
+#include <json.hpp>
+
+// TODO: finish this function
+static auto _get_post(nlohmann::json& post) -> Post
+{
+    Post post_obj;
+
+    if (!post["no"].empty()) {
+        post_obj.postnumber = post["no"].get<int>();
+    }
+
+    if (!post["name"].empty()) {
+        post_obj.name = post["name"].get<std::string>();
+    }
+
+    return post_obj;
+}
+
+static auto _get_thread(nlohmann::json& thread) -> Thread
+{
+    Thread thread_obj;
+
+    for (nlohmann::json& post : thread["posts"]) {
+        auto post_info = _get_post(post);
+        thread_obj.posts.push_back(post_info);
+    }
+
+    return thread_obj;
+}
+
+static auto get_thread(std::string const& board, std::string const& thread) -> Thread
+{
+    auto website = "http://a.4cdn.org/" + board + "/thread/" + thread + ".json";
+    auto download = channer::download_json(website.c_str());
+
+    auto json = nlohmann::json::parse(download);
+    return _get_thread(json);
+}
