35b58b863c6bb5d57ca54b2040aacc370c7796f3
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-09 23:01:52 +0000

    Request repo

---
+++ include/Repository.hpp
@@ -0,0 +1,16 @@
+#ifndef _REPOSITORY_HPP_
+#define _REPOSITORY_HPP_
+
+#include <Catalog.hpp>
+#include <Constants.hpp>
+#include <Download.hpp>
+#include <JsonUtils.hpp>
+#include <Thread.hpp>
+
+namespace channer::repo
+{
+auto get_thread(std::string const& board, std::string const& thread, bool file_only = false) -> Thread;
+auto get_catalog(std::string const& board) -> Catalog;
+}
+
+#endif
---
+++ src/Repository.cpp
@@ -0,0 +1,30 @@
+#include <Repository.hpp>
+
+namespace channer::repo
+{
+auto get_thread(std::string const& board, std::string const& thread, bool file_only) -> Thread
+{
+    auto website = channer::endpoints::URL_THREAD + board + channer::endpoints::TYPE_THREAD + thread + channer::endpoints::FORMAT_JSON;
+    auto download = channer::download_json(website.c_str());
+
+    if (download == "") {
+        return Thread();
+    }
+
+    auto json = nlohmann::json::parse(download);
+    return channer::json::get_thread(json, board, file_only);
+}
+
+auto get_catalog(std::string const& board) -> Catalog
+{
+    auto website = channer::endpoints::URL_THREAD + board + "/catalog" + channer::endpoints::FORMAT_JSON;
+    auto download = channer::download_json(website.c_str());
+
+    if (download == "") {
+        throw std::exception();
+    }
+
+    auto json = nlohmann::json::parse(download);
+    return channer::json::get_catalog(json, board);
+}
+}
