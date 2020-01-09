d8ebf86d49a8a7d3fc275656b3512a2454e070cc
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-09 16:55:59 +0000

    execute_request templates constexpr/non-constexpr

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,11 +1,54 @@
 #ifndef _UTILS_HPP_
 #define _UTILS_HPP_
 
+#include <future>
+#include <iostream>
 #include <string>
+#include <utility>
 
 namespace channer::utils
 {
-	auto replace(std::string& str, std::string_view const from, std::string_view const to) -> void;
+auto replace(std::string& str, std::string_view const from, std::string_view const to) -> void;
+}
+
+template<typename T, typename F>
+auto execute_request(
+    F&& request,
+    std::function<void(T)> success,
+    std::function<void(void)> failure) -> void
+{
+    try {
+        auto res = std::async(std::launch::async,
+                              [request = std::forward<F>(request)](auto&&... args) -> T {
+                                  return request(std::forward<decltype(args)>(args)...);
+                              });
+
+        success(res.get());
+    } catch (std::exception const& e) {
+        failure();
+    }
+}
+
+template<typename T, typename F, typename G, typename H>
+constexpr auto execute_request(
+    F&& request,
+    G&& success,
+    H&& failure) -> void
+{
+    try {
+        auto res = std::async(std::launch::async,
+                              [request = std::forward<F>(request)](auto const&&... args) -> T {
+                                  return request(std::forward<decltype(args)>(args)...);
+                              });
+
+        [success = std::forward<G>(success)](auto const&&... args) {
+            success(std::forward<decltype(args)>(args)...);
+        }(res.get());
+    } catch (std::exception const& e) {
+        [failure = std::forward<H>(failure)](auto const&&... args) {
+            failure(std::forward<decltype(args)>(args)...);
+        }();
+    }
 }
 
 #endif
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -1,18 +1,35 @@
 #include <Client.hpp>
+#include <Utils.hpp>
 #include <future>
 
 auto main() -> int
 {
     //auto thread = get_thread("g", "74238065");
 
-    get_catalog(
-        "g",
-        [](bool res) {
-            std::printf("Success\n");
-        },
-        [](bool res) {
-            std::printf("Failure\n");
-        });
+    // get_catalog(
+    //     "g",
+    //     [](bool res) {
+    //         std::printf("Success\n");
+    //     },
+    //     [](bool res) {
+    //         std::printf("Failure\n");
+    //     });
+
+    // execute_request<Catalog>([]() -> Catalog { return Catalog(); },
+    //                          [](Catalog arg) {
+    //                              std::printf("Success\n");
+    //                          },
+    //                          []() {
+    //                              std::printf("Failure\n");
+    //                          });
+
+    execute_request<Catalog>([]() -> Catalog { return get_catalog("g"); },
+                             [](Catalog arg) {
+                                 std::printf("Success: %d\n", arg.catalog_entries.size());
+                             },
+                             []() {
+                                 std::printf("Failure\n");
+                             });
 
     return 0;
 }
