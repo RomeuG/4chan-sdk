5aeeef18500aabd9d8008f6acf910a79cbb9bbb5
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-14 18:12:41 +0000

    Board search (WIP)

--- src/Repository.cpp
+++ src/Repository.cpp
@@ -41,20 +41,38 @@ auto get_boards() -> Boards
 	return channer::json::get_boards(json);
 }
 
-// auto search_board() -> std::vector<Board>
-// {
-// 	auto website = channer::endpoints::URL_THREAD + std::string("boards") + channer::endpoints::FORMAT_JSON;
-// 	auto download = channer::download_json(website.c_str());
+template<typename T>
+static auto validate_option(bool &valid) -> bool
+{
 
-// 	if (download == "") {
-// 		throw std::runtime_error("Download error");
-// 	}
+}
 
-// 	auto json = nlohmann::json::parse(download);
-// 	auto boards = channer::json::get_boards(json).boards;
+auto search_board(Board desired) -> std::vector<Board>
+{
+	auto website = channer::endpoints::URL_THREAD + std::string("boards") + channer::endpoints::FORMAT_JSON;
+	auto download = channer::download_json(website.c_str());
 
-// 	auto found = std::find_if(std::begin(boards), std::end(boards), [&](Board const & board) {
+	if (download == "") {
+		throw std::runtime_error("Download error");
+	}
 
-// 	});
-// }
+	auto json = nlohmann::json::parse(download);
+	auto boards = channer::json::get_boards(json).boards;
+
+	auto found = std::find_if(std::begin(boards), std::end(boards), [&](Board const & board) {
+		auto valid = false;
+
+		if (desired.board != "") {
+			valid = desired.board == board.board;
+		}
+
+		if (desired.bump_limit != -1) {
+			valid = desired.bump_limit == board.bump_limit;
+		}
+
+		if (desired.country_flags != -1) {
+
+		}
+	});
+}
 }
