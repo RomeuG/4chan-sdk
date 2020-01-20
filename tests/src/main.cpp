0856087a4059e87f90eed755d9ee4faa49c2e140
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-20 22:41:50 +0000

    search_board() test case

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -189,3 +189,36 @@ TEST_CASE("Get /po/ threads")
 
         });
 };
+
+TEST_CASE("Search a board and get correct result")
+{
+    auto desired = Board();
+    desired.board = "an";
+
+    channer::search_board(
+        desired,
+        [](std::vector<Board> arg) {
+            SECTION("Fifth board information is correct")
+            {
+                auto b = arg[0];
+
+                REQUIRE(b.board == "an");
+                REQUIRE(b.title == "Animals & Nature");
+                REQUIRE(b.ws_board == 1);
+                REQUIRE(b.per_page == 15);
+                REQUIRE(b.pages == 10);
+                REQUIRE(b.max_filesize == 4194304);
+                REQUIRE(b.max_webm_filesize == 3145728);
+                REQUIRE(b.max_comment_chars == 2000);
+                REQUIRE(b.max_webm_duration == 120);
+                REQUIRE(b.bump_limit == 310);
+                REQUIRE(b.image_limit == 150);
+                REQUIRE(b.cooldowns.threads == 600);
+                REQUIRE(b.cooldowns.replies == 60);
+                REQUIRE(b.cooldowns.images == 60);
+                REQUIRE(b.is_archived == 1);
+            }
+        },
+        [](std::runtime_error const& e) {
+        });
+}
