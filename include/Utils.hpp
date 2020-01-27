521aa99b7ddf075cbc3fc400e3a01cb7e5ef2f2f
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-27 18:06:50 +0000

    Utils to create file dir string and file url

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -8,10 +8,13 @@
 #include <string>
 #include <utility>
 
+#include <Constants.hpp>
+
 namespace channer::utils
 {
 auto replace(std::string& str, std::string_view const from, std::string_view const to) -> void;
-auto create_media_url(std::string const& dir, long long const tim, std::string const& extension) -> std::string;
+auto create_media_path(std::string const& dir, long long const tim, std::string const& extension) -> std::string;
+auto create_media_url(std::string const& board, long long const tim, std::string const& extension) -> std::string;
 #ifdef MOCKDATA
 auto load_file(std::string const& path) -> std::string;
 #endif
