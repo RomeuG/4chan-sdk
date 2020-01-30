97b07408584c3ce637db257db96d07d3cc122ead
Romeu Vieira <romeu.bizz@gmail.com>
2020-01-30 22:40:06 +0000

    Refer to previous commit message

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,6 +1,7 @@
 #ifndef _UTILS_HPP_
 #define _UTILS_HPP_
 
+#include <Constants.hpp>
 #include <filesystem>
 #include <fstream>
 #include <future>
@@ -8,12 +9,11 @@
 #include <string>
 #include <utility>
 
-#include <Constants.hpp>
-
 namespace channer::utils
 {
 auto replace(std::string& str, std::string_view const from, std::string_view const to) -> void;
 auto create_media_path(std::string const& dir, long long const tim, std::string const& extension) -> std::string;
+auto create_media_path(std::string const& dir, std::string const& filename, std::string const& extension) -> std::string;
 auto create_media_url(std::string const& board, long long const tim, std::string const& extension) -> std::string;
 #ifdef MOCKDATA
 auto load_file(std::string const& path) -> std::string;
