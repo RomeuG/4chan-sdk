1768004d8800469cc7f4e7f41c3f93caca4876d5
Romeu Vieira <romeu.bizz@gmail.com>
2021-09-12 18:14:49 +0100

    HTML parse vastly improved

--- fourchan-sdk/src/core/html.rs
+++ fourchan-sdk/src/core/html.rs
@@ -1,3 +1,6 @@
+use html5ever::tendril::fmt::UTF8;
+use html5ever::tendril::Tendril;
+use html5ever::QualName;
 use select::document::Document;
 use select::node::{Data, Raw};
 
@@ -5,9 +8,22 @@ use select::node::{Data, Raw};
 pub enum TextType {
     PlainText(String),
     Italics(String),
-    Link(String),
+    Link(String, Option<String>),
     Quote(String),
     NewLine,
+}
+
+pub(crate) fn html_get_property_value(
+    property: &str,
+    properties: &Vec<(QualName, Tendril<UTF8>)>,
+) -> Option<String> {
+    for prop in properties {
+        if &prop.0.local == property {
+            return Some(prop.1.to_string());
+        }
+    }
+
+    return None;
 }
 
 pub fn html_parse_node(document: &Document, node: &Raw) -> Vec<TextType> {
@@ -18,9 +34,23 @@ pub fn html_parse_node(document: &Document, node: &Raw) -> Vec<TextType> {
 
         while let Some(sibling) = _sibling {
             match &sibling.data {
-                Data::Text(t) => {
-                    vec.push(TextType::PlainText(t.to_string()));
-                }
+                Data::Text(t) => match &node.data {
+                    Data::Element(parent_qual, props) => {
+                        if &parent_qual.local == "i" {
+                            vec.push(TextType::Italics(t.to_string()));
+                        } else if &parent_qual.local == "a" {
+                            let href: Option<String> = html_get_property_value("href", &props);
+                            vec.push(TextType::Link(t.to_string(), href));
+                        } else if &parent_qual.local == "span" {
+                            vec.push(TextType::Quote(t.to_string()));
+                        } else {
+                            vec.push(TextType::PlainText(t.to_string()));
+                        }
+                    }
+                    _ => {
+                        vec.push(TextType::PlainText(t.to_string()));
+                    }
+                },
                 Data::Element(qual, _) => {
                     if &qual.local == "br" {
                         vec.push(TextType::NewLine);
@@ -51,11 +81,10 @@ pub fn html_parse_node(document: &Document, node: &Raw) -> Vec<TextType> {
 
 pub fn html_parse_post(text: &str) {
     let document = Document::from(text);
-    for item in &document.nodes {
-        println!("{:?}", item);
-    }
 
-    println!("\n\n\n\n");
+    // for item in &document.nodes {
+    //     println!("{:?}", item);
+    // }
 
     let body_node = document.nodes.get(2).unwrap();
     let vec_text = html_parse_node(&document, body_node);
